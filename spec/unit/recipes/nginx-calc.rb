cb72f750d39962c1884f365de5af6fbf4994b0d4
James Lewis <jamesl@thoughtworks.com>
2016-02-13 11:58:50 +1000

    expect cookbook to include apt recipe

--- spec/unit/recipes/nginx-calc.rb
+++ spec/unit/recipes/nginx-calc.rb
@@ -3,6 +3,10 @@ require_relative '../../spec_helper.rb'
 describe 'nginx-calc::default' do
   let (:chef_run) { ChefSpec::SoloRunner.converge(described_recipe) }
 
+  it 'installs apt recipe' do
+    expect(chef_run).to include_recipe('apt')
+  end
+
   it 'installs nginx' do
     expect(chef_run).to install_package('nginx')
   end
